bd34fa38896a8da84bd7632bc520462f7213dac3
Tran Duc Khai / チャン.ドゥック.カイ <89479879+duckhai2323@users.noreply.github.com>
2023-11-04 03:15:03 +0700

    FixBug#1_UpdateFilterLogic

--- lib/pages/alldesign/alldesign_controller.dart
+++ lib/pages/alldesign/alldesign_controller.dart
@@ -1,3 +1,4 @@
+import 'dart:ffi';
 import 'dart:ui';
 
 import 'package:flutter/material.dart';
@@ -7,46 +8,53 @@ import 'package:thietthach_app/pages/alldesign/itemfilter.dart';
 
 class AllDesignController extends GetxController{
   List<ItemFilter> listFilter0 = <ItemFilter>[].obs;
-  List<ItemFilter> listFilter1 = <ItemFilter>[].obs;
-  List<ItemFilter> listFilter2 = <ItemFilter>[].obs;
+  List<ItemFilter> list = <ItemFilter>[].obs;
+  Map<String,ItemFilter> mapFilter = <String,ItemFilter>{}.obs;
   @override
   void onInit(){
     super.onInit();
-    listFilter1.add(ItemFilter('Room',Colors.black,"room"));
-    listFilter1.add(ItemFilter('Style',Colors.black,"style"));
-    listFilter1.add(ItemFilter('Budget',Colors.black,"budget"));
-    listFilter1.add(ItemFilter('Size',Colors.black,"size"));
-    listFilter1.add(ItemFilter('Color',Colors.black,"color"));
-    listFilter0 = List.from(listFilter1);
+    ConstListFiler();
+  }
+
+  void ConstListFiler(){
+    listFilter0.clear();
+    listFilter0.add(ItemFilter('Room',Colors.black,"Room"));
+    listFilter0.add(ItemFilter('Style',Colors.black,"Style"));
+    listFilter0.add(ItemFilter('Budget',Colors.black,"Budget"));
+    listFilter0.add(ItemFilter('Size',Colors.black,"Size"));
+    listFilter0.add(ItemFilter('Color',Colors.black,"Color"));
   }
 
   void ShowDialogFilter(BuildContext context, String key){
-    switch(key){
-      case "room":{
-        List<String> list = [];
-        list.add("Kitchen");
-        list.add("Bath");
-        list.add("Bedroom");
-        list.add("Living");
-        list.add("Dining");
-        list.add("Outdoor");
-        list.add("Baby&Kids");
-        list.add("HomeOffice");
-        list.add("Exterior");
-        list.add("Bedroom");
-        list.add("Living");
-        list.add("Dining");
-        list.add("Outdoor");
-        list.add("Baby&Kids");
-        list.add("HomeOffice");
-        list.add("Exterior");
-        DialogFilter(context,list,'Room',key);
+    if(!mapFilter.containsKey(key)){
+      switch(key){
+        case "Room":{
+          list.clear();
+          list.add( ItemFilter("Kitchen",Colors.black,"false"));
+          list.add(ItemFilter("Bath",Colors.black,"false"));
+          list.add(ItemFilter("Bedroom",Colors.black,"false"));
+          list.add(ItemFilter("Living",Colors.black,"false"));
+          list.add(ItemFilter("Dining",Colors.black,"false"));
+          list.add(ItemFilter("Outdoor",Colors.black,"false"));
+          list.add(ItemFilter("Baby&Kids",Colors.black,"false"));
+          list.add(ItemFilter("HomeOffice",Colors.black,"false"));
+          list.add(ItemFilter("Exterior",Colors.black,"false"));
+          list.add(ItemFilter("Bedroom",Colors.black,"false"));
+          list.add(ItemFilter("Living",Colors.black,"false"));
+          list.add(ItemFilter("Dining",Colors.black,"false"));
+          list.add(ItemFilter("Baby&Kids",Colors.black,"false"));
+          list.add(ItemFilter("HomeOffice",Colors.black,"false"));
+          list.add(ItemFilter("Exterior",Colors.black,"false"));
+          DialogFilter(context,list,key);
+        }
+        break;
       }
-      break;
+    } else {
+      DialogFilter(context,list,key);
     }
   }
 
-  void DialogFilter(BuildContext context, List<String> list,String title,String key){
+  void DialogFilter(BuildContext context, List<ItemFilter> list,String key){
     showModalBottomSheet(
         context: context,
         isScrollControlled: true,
@@ -101,7 +109,7 @@ class AllDesignController extends GetxController{
                           ),
 
                           Text(
-                            title,
+                            key,
                             style: TextStyle(
                               color: Colors.black,
                               fontWeight: FontWeight.w600,
@@ -123,7 +131,7 @@ class AllDesignController extends GetxController{
 
                     Divider(),
 
-                    Expanded(
+                    Obx(() => Expanded(
                       child: ListView.builder(
                         scrollDirection: Axis.vertical,
                         itemCount: list.length,
@@ -131,37 +139,64 @@ class AllDesignController extends GetxController{
                         itemBuilder: (_,index){
                           return InkWell(
                             onTap: (){
-                              listFilter2.clear();
-                              listFilter0.clear();
-                              listFilter2.add(ItemFilter('Layout',Colors.black,"layout"));
-                              listFilter2.add(ItemFilter('Type',Colors.black,"type"));
-                              listFilter2.add(ItemFilter('Number of Islands',Colors.black,"numberofislands"));
-                              listFilter2.add(ItemFilter('Cabinet Style',Colors.black,"cabinetstyle"));
-                              listFilter2.add(ItemFilter('Cabinet Finish',Colors.black,"cabinetfinish"));
-                              listFilter2.add(ItemFilter('Counter Material',Colors.black,"countermaterial"));
-                              listFilter2.add(ItemFilter('Counter Color',Colors.black,"countercolor"));
-                              listFilter2.add(ItemFilter('Backplash Material',Colors.black,"blackpashmaterial"));
-                              listFilter2.add(ItemFilter('Appliance Finish',Colors.black,"appliance"));
-                              listFilter2.add(ItemFilter('Sink',Colors.black,"sink"));
-                              listFilter2.add(ItemFilter('Floor Material',Colors.black,"floormaterial"));
-                              listFilter2.add(ItemFilter('Floor Color',Colors.black,"Floor Color"));
-                              listFilter2.add(ItemFilter('Ceiling Design',Colors.black,"ceiling"));
-                              listFilter0 = listFilter1 + listFilter2;
+                              ConstListFiler();
+                              switch(list[index].text){
+                                case "Kitchen":{
+                                  listFilter0.add(ItemFilter('Layout',Colors.black,"layout"));
+                                  listFilter0.add(ItemFilter('Type',Colors.black,"type"));
+                                  listFilter0.add(ItemFilter('Number of Islands',Colors.black,"numberofislands"));
+                                  listFilter0.add(ItemFilter('Cabinet Style',Colors.black,"cabinetstyle"));
+                                  listFilter0.add(ItemFilter('Cabinet Finish',Colors.black,"cabinetfinish"));
+                                  listFilter0.add(ItemFilter('Counter Material',Colors.black,"countermaterial"));
+                                  listFilter0.add(ItemFilter('Counter Color',Colors.black,"countercolor"));
+                                  listFilter0.add(ItemFilter('Backplash Material',Colors.black,"blackpashmaterial"));
+                                  listFilter0.add(ItemFilter('Appliance Finish',Colors.black,"appliance"));
+                                  listFilter0.add(ItemFilter('Sink',Colors.black,"sink"));
+                                  listFilter0.add(ItemFilter('Floor Material',Colors.black,"floormaterial"));
+                                  listFilter0.add(ItemFilter('Floor Color',Colors.black,"Floor Color"));
+                                  listFilter0.add(ItemFilter('Ceiling Design',Colors.black,"ceiling"));
+                                }
+                                break;
+                              }
+                              if(mapFilter.containsKey(key)){
+                                ItemFilter object2 = ItemFilter(mapFilter[key]?.text,Colors.black,"false");
+                                int temp = list.indexOf(mapFilter[key]!);
+                                list.insert(temp, object2);
+                                list.removeAt(temp+1);
+                              }
+                              ItemFilter object = ItemFilter(list[index].text??"",AppColors.backgroundIntro , "true");
+                              int k = list.indexOf(list[index]);
+                              list.insert(k,object);
+                              list.removeAt(k+1);
+                              mapFilter[key] = list[index];
                             },
                             child: Padding(
-                              padding: EdgeInsets.only(left: 15,top: 15,bottom: 10),
-                              child: Text(
-                                list[index],
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.w500
-                                ),
+                              padding: EdgeInsets.only(left: 15,top: 15,bottom: 10,right: 15),
+                              child: Row(
+                                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                                mainAxisSize: MainAxisSize.max,
+                                crossAxisAlignment: CrossAxisAlignment.center,
+                                children: [
+                                  Text(
+                                    list[index].text??"",
+                                    style: TextStyle(
+                                        color: list[index].color,
+                                        fontSize: 18,
+                                        fontWeight: FontWeight.w500
+                                    ),
+                                  ),
+
+                                  Visibility(
+                                    visible: list[index].key == "true"?true:false,
+                                    child: Icon( IconData(0xe156, fontFamily: 'MaterialIcons'),color: list[index].color),
+                                  ),
+                                ],
                               ),
                             ),
                           );
                         },
                       ),
-                    ),
+                    )),
 
                     Container(
                       height: 55,
